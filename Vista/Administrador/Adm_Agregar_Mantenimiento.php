af8ced142d3e084b2b650b466eddaefdbf0199b8
PasacheVip <pasache-2002@hotmail.com>
2024-06-11 08:32:17 -0500

    v3

--- Vista/Administrador/Adm_Agregar_Mantenimiento.php
+++ Vista/Administrador/Adm_Agregar_Mantenimiento.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Mantenimiento</title>
     <!-- ===== CSS ===== -->
-    <link rel="stylesheet" href="../../Modelo/Archivos_CSS/Gestion_Mantenimiento.css">
+    <link rel="stylesheet" href="../../Modelo/Archivos_CSS/Style_ForMantenimiento.css">
 </head>
 <body>
     <?php
--- Vista/Administrador/Adm_Gestion_Mantenimientos.php
+++ Vista/Administrador/Adm_Gestion_Mantenimientos.php
@@ -9,35 +9,42 @@
     $conn = new Conexion_BD();
 ?>
 
-<?php
-    // Iniciar la sesión si no está iniciada
-    session_start();
-
-    // Incluir el archivo de conexión a la base de datos
-    require_once '../../Controlador/Utilidades/Conexion_BD.php';
-
-    // Crear una instancia de la clase Conexion_BD
-    $conn = new Conexion_BD();
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Mantenimientos</title>
+
+    <!-- Icono de Pagina -->
+    <link rel="icon" href="../../Modelo/Archivos_Media/Color_LogoTipo.png" type="image/png">
+
+    <!-- ==== BOX-ICONS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     <!-- ===== CSS ===== -->
     <link rel="stylesheet" href="../../Modelo/Archivos_CSS/Gestion_Mantenimiento.css">
+    <link rel="stylesheet" href="../../Modelo/Archivos_CSS/Style_table_Gestion.css">
+
 </head>
 <body class="Manteni-body">
-    <h1 class="Manteni-h1">Gestión de Mantenimientos</h1>
-    <a href="Adm_Agregar_Mantenimiento.php" class="Manteni-a">Agregar Mantenimiento</a>
-    <table class="Manteni-table">
+    <!-- Mostrar el nombre dentro del h2 -->
+    <div class="container">
+        <h2>Tabla Gestion de Mantenimientos</h2>    
+    </div>
+    
+    <!-- Botón para agregar nuevo usuario -->
+    <button role="button" class="button-name" onclick="location.href='Adm_Agregar_Mantenimiento.php'">Registrar Nuevo Mantenimiento +</button>
+    <button role="button" class="button-name" onclick="location.href='Adm_Gestion_Mantenimientos.php'">Actualizar</button>
+
+    <!-- Tabla -->
+    <div class="table-wrapper"> 
+    <table>
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Responsable</th>
-                <th>Descripción</th>
+                <th style="text-align: center">ID</th>
+                <th style="text-align: left">Responsable</th>
+                <th style="text-align: left">Descripción</th>
                 <th>Fecha</th>
                 <th>Tipo de Mantenimiento</th>
                 <th>Acciones</th>
@@ -52,12 +59,14 @@
             if ($result) {
                 while ($row = $result->fetch_assoc()) {
                     echo "<tr>";
-                    echo "<td>" . $row['id_mantenimiento'] . "</td>";
-                    echo "<td>" . $row['encargado'] . "</td>";
-                    echo "<td>" . $row['descripcion'] . "</td>";
+                    echo "<td style='text-align: center;'>" . $row['id_mantenimiento'] . "</td>";
+                    echo "<td style='text-align: left;'>" . $row['encargado'] . "</td>";
+                    echo "<td style='text-align: left;'>" . $row['descripcion'] . "</td>";
                     echo "<td>" . $row['fecha'] . "</td>";
                     echo "<td>" . $row['tipo_mantenimiento'] . "</td>";
-                    echo "<td><a href='Adm_Editar_Mantenimiento.php?id=" . $row['id_mantenimiento'] . "' class='Manteni-a'>Editar</a> | <a href='MySQL_Borrar_Mantenimiento.php?id=" . $row['id_mantenimiento'] . "' class='Manteni-a'>Eliminar</a></td>";
+                    echo "<td><a href='Adm_Editar_Mantenimiento.php?id=" . $row['id_mantenimiento'] . "'><i class='bx bx-edit bx-sm'></i></a>
+                              <a href='MySQL_Borrar_Mantenimiento.php?id=" . $row['id_mantenimiento'] . "'><i class='bx bx-trash bx-sm'></i></a>
+                          </td>";
                     echo "</tr>";
                 }
             } else {
